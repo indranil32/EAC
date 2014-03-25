41d4f30220596400ca0bfc6d4a8e25b0d588094b
Indranil <indranil32@gmail.com>
2014-03-25 17:26:47 +0530

    RegistryManager

---
+++ RegistryManager/src/com/cisco/registry/IRegistry.java
@@ -0,0 +1,23 @@
+/**
+ * @Title IRegistry.java
+ *
+ * @Description  
+ *
+ * @Project RegistryManager
+ * @Author imajumde
+ * @Version 1.0
+ * @Copyright (c) 2001, 2011-2012 by Cisco Systems, Inc.
+ *
+ * @History Mar 14, 2014
+ */
+package com.cisco.registry;
+
+/**
+ * IRegistry - 
+ *
+ */
+public interface IRegistry {
+    public String getProgram(int usecase);
+    public String getInputPath();
+    public String getOutpath();
+}
---
+++ RegistryManager/src/com/cisco/registry/mso/MSORegistry.java
@@ -0,0 +1,23 @@
+/**
+ * @Title MSORegistry.java
+ *
+ * @Description  
+ *
+ * @Project RegistryManager
+ * @Author imajumde
+ * @Version 1.0
+ * @Copyright (c) 2001, 2011-2012 by Cisco Systems, Inc.
+ *
+ * @History Mar 14, 2014
+ */
+package com.cisco.registry.mso;
+
+import com.cisco.registry.IRegistry;
+
+/**
+ * MSORegistry - 
+ *
+ */
+public abstract class MSORegistry implements IRegistry {
+
+}
