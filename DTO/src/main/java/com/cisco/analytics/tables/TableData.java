bf7f315e521f2dfe6c29f5af590a8b8efc0d6afe
Indranil <indranil32@gmail.com>
2014-03-28 12:42:29 +0530

    adding missed files

---
+++ DTO/src/main/java/com/cisco/analytics/tables/TableData.java
@@ -0,0 +1,51 @@
+/*
+######################################################
+#
+# @Copyright (c) 2014 by Cisco Systems, Inc.
+#
+# @Title TableData.java
+# @Description This class represents the data model of a Table Widget's data
+# @Version 1.0
+# @author  vcbu_ui_team@cisco.com
+#
+######################################################
+*/
+package com.cisco.analytics.tables;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class TableData {
+
+	private List<Map<String,String>> items;
+	private String identifier;
+	
+	public List<Map<String, String>> getItems() {
+		if (items == null) {
+			items = new ArrayList<Map<String, String>>();
+        }
+		return items;
+	}
+
+	
+	public void setItems(List<Map<String, String>> items) {
+		this.items = items;
+	}
+
+	public String getIdentifier() {
+		return identifier;
+	}
+
+	public void setIdentifier(String identifier) {
+		this.identifier = identifier;
+	}
+
+
+	@Override
+	public String toString() {
+		return "TableData [items=" + items + ", identifier=" + identifier + "]";
+	}
+
+	
+}
